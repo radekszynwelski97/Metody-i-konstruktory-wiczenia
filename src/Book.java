e057427aa9a11dd6470bb6c63ab3e80f6279f89e
Radek Szynwelski <121770151+radekszynwelski97@users.noreply.github.com>
2023-01-11 21:10:06 +0100

    Konstruktory , przeciążanie metod oraz słowo this

--- src/Book.java
+++ src/Book.java
@@ -5,6 +5,13 @@ public class Book {
                 "1992" , 294 , "212121" , "Red");
         library1.printInfo();
 
+        Library library2 = new Library("Magiczna moc perfum ", "Penny Jordan" , "1255" ,
+                450 , "2112121" , "Yellow");
+        library2.printInfo();
+        Library library3 = new Library("Najbogatszy Hiszpan " , "Susan Stephens " , "1252" ,
+                410 ,  "Blue");
+        library3.printInfo();
+
 
 
     }
--- src/Library.java
+++ src/Library.java
@@ -7,16 +7,17 @@ public class Library {
     String numberIsbn ;
     String color ;
 
-    Library(String bookTittle, String bookAuthor, String bookYearOfPublisment, int bookPages, String bookNumberISBN,
-         String bookColor) {
-        tittle=bookTittle;
-        author=bookAuthor;
-        yearOfPublisment=bookYearOfPublisment;
-        pages=bookPages;
-        numberIsbn=bookNumberISBN;
-        color=bookColor;
+     Library(String tittle, String author, String yearOfPublisment, int pages, String numberIsbn) {
+        this.tittle = tittle;
+        this.author = author;
+        this.yearOfPublisment = yearOfPublisment;
+        this.pages = pages;
+        this.numberIsbn = numberIsbn;
+    }
 
-
+     Library(String tittle, String author, String yearOfPublisment, int pages, String numberIsbn, String color) {
+        this(tittle , author , yearOfPublisment , pages  , numberIsbn);
+        this.color = color;
     }
 
     void printInfo(){
